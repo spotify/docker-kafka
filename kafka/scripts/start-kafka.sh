11358b76a8ae87eb05af5a49aa46cd204c0c216e
Marc Bruggmann <bruggmann.marc@gmail.com>
2015-03-28 14:03:20 +0100

    Make advertised host/port configurable.

--- kafka/scripts/start-kafka.sh
+++ kafka/scripts/start-kafka.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
 # Optional ENV variables:
-# * HELIOS_PORT_kafka: the external hostname and port, e.g. "awseu3-heliosciagent-a1.spotify.net:9092"
+# * ADVERTISED_HOST: the external ip for the container, e.g. `boot2docker ip`
+# * ADVERTISED_PORT: the external port for Kafka, e.g. 9092
 # * ZK_CHROOT: the zookeeper chroot that's used by Kafka (without / prefix), e.g. "kafka"
 
-# Set the external host and port
+# Configure advertised host/port if we run in helios
 if [ ! -z "$HELIOS_PORT_kafka" ]; then
     ADVERTISED_HOST=`echo $HELIOS_PORT_kafka | cut -d':' -f 1 | xargs -n 1 dig +short | tail -n 1`
     ADVERTISED_PORT=`echo $HELIOS_PORT_kafka | cut -d':' -f 2`
+fi
 
+# Set the external host and port
+if [ ! -z "$ADVERTISED_HOST" ]; then
+    echo "advertised host: $ADVERTISED_HOST"
     sed -r -i "s/#(advertised.host.name)=(.*)/\1=$ADVERTISED_HOST/g" $KAFKA_HOME/config/server.properties
+fi
+if [ ! -z "$ADVERTISED_PORT" ]; then
+    echo "advertised port: $ADVERTISED_PORT"
     sed -r -i "s/#(advertised.port)=(.*)/\1=$ADVERTISED_PORT/g" $KAFKA_HOME/config/server.properties
 fi
 
