fc8cdbd2e23a5cac21e7138d07ea884b4309c59a
eugenemi <eugenemi@gmail.com>
2016-12-08 05:52:22 -0800

    upgrade to kafka 0.10.1.0 (#60)
    * KAFKA_VERSION 0.10.1.0
    
    * adjustment for kafka 10 config change

--- kafka/scripts/start-kafka.sh
+++ kafka/scripts/start-kafka.sh
@@ -17,11 +17,19 @@ fi
 # Set the external host and port
 if [ ! -z "$ADVERTISED_HOST" ]; then
     echo "advertised host: $ADVERTISED_HOST"
-    sed -r -i "s/#(advertised.host.name)=(.*)/\1=$ADVERTISED_HOST/g" $KAFKA_HOME/config/server.properties
+    if grep -q "^advertised.host.name" $KAFKA_HOME/config/server.properties; then
+        sed -r -i "s/#(advertised.host.name)=(.*)/\1=$ADVERTISED_HOST/g" $KAFKA_HOME/config/server.properties
+    else
+        echo "advertised.host.name=$ADVERTISED_HOST" >> $KAFKA_HOME/config/server.properties
+    fi
 fi
 if [ ! -z "$ADVERTISED_PORT" ]; then
     echo "advertised port: $ADVERTISED_PORT"
-    sed -r -i "s/#(advertised.port)=(.*)/\1=$ADVERTISED_PORT/g" $KAFKA_HOME/config/server.properties
+    if grep -q "^advertised.port" $KAFKA_HOME/config/server.properties; then
+        sed -r -i "s/#(advertised.port)=(.*)/\1=$ADVERTISED_PORT/g" $KAFKA_HOME/config/server.properties
+    else
+        echo "advertised.port=$ADVERTISED_PORT" >> $KAFKA_HOME/config/server.properties
+    fi
 fi
 
 # Set the zookeeper chroot
